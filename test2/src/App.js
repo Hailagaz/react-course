c12daec21a5593af2101d6e5fc65ae576a870025
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-02-23 17:34:36 +0200

    fix, add test app

---
+++ test2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+const name = 'Neo';
+const elem = 'Hello ' + name + '. Its matrix.';
+
+
+function App() {
+  return (
+    <div className="App">
+		  <h1>{elem}</h1>
+    </div>
+  );
+}
+
+export default App;
