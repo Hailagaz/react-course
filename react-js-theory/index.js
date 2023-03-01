9fd32b9c4e1124fa82e7a5c9ef8240dd7d221636
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-03-01 16:13:40 +0200

    fix

--- react-js-theory/index.js
+++ react-js-theory/index.js
@@ -387,5 +387,17 @@ class EssayForm extends React.Component {
 		alert('Essay was sent:' + this.state.value);
 		event.preventDefault();
 	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Essay:
+					<textarea value={this.state.value} onChange={this.handleChange} />
+				</label>
+				<input type='submit' value='Send'/>
+			</form>
+		);
+	}
 }
 
