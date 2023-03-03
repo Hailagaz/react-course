0b11b5b40928aeadb2493764fbfc5252ab210a52
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-03-03 12:54:36 +0200

    fix

--- react-js-theory/index.js
+++ react-js-theory/index.js
@@ -525,28 +525,46 @@ function BoilingVerdict(props) {
 	return <p>Water will not boil</p>
 }
 
-class Calculator extends React.Component {
+
+const scaleNames = {
+	c: 'Celsius',
+	f: 'Fahrengeit'
+}
+
+class TemperatureInput extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
-		this.state = { temperature: '0' };
+		this.state = {temperature: ''};
 	}
 
 	handleChange(event) {
-		this.setState({ temperature: event.target.value });
+		this.setState({temperature: event.target.value});
 	}
 
 	render() {
 		const temperature = this.state.temperature;
+		const scale = this.props.scale;
 		return (
 			<fieldset>
-				<legend>Write temperature in celsius degrees</legend>
+				<legend>Enter temperature in degrees of {scaleNames[scale]}: </legend>
 				<input
-					value={temperature}
 					onChange={this.handleChange}
+					value={temperature}
 				/>
-				<BoilingVerdict celsius={parseFloat(temperature)} />
 			</fieldset>
+		);
+	}
+}
+
+class Calculator extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<TemperatureInput scale='f' />
+				<TemperatureInput scale='c' />
+			</div>
 		);
 	}
 }
