7ba1ded9f42401a38b5628d331b9a18940c48b8e
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-03-03 13:28:16 +0200

    fix

--- react-js-theory/index.js
+++ react-js-theory/index.js
@@ -528,7 +528,7 @@ function BoilingVerdict(props) {
 
 const scaleNames = {
 	c: 'Celsius',
-	f: 'Fahrengeit'
+	f: 'Fahrenheit'
 }
 
 class TemperatureInput extends React.Component {
@@ -569,4 +569,27 @@ class Calculator extends React.Component {
 }
 
 const root12 = ReactDOM.createRoot(document.getElementById('root12'));
-root12.render(<Calculator/>);
+root12.render(<Calculator />);
+
+
+
+
+
+
+function toCelsius(fahrenheit) {
+	return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+	return (celsius * 9 / 5) + 32;
+}
+
+function TryConvert(temperature, convert) {
+	const input = parseFloat(temperature);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
+}
