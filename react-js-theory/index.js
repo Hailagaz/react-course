3a94765ca3d9ab5c310f37ba2502225d19f25623
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-03-06 13:19:01 +0200

    fix

--- react-js-theory/index.js
+++ react-js-theory/index.js
@@ -800,8 +800,33 @@ root14.render(<SignUpDialog/>);
 
 
 
+class ProductCategoryRow extends React.Component {
+	render() {
+		const category = this.props.category;
+		return (
+			<tr>
+				<th colSpan="2">{category}</th>
+			</tr>
+		);
+	}
+}
 
-
+class ProductRow extends React.Component {
+	render() {
+		const product = this.props.product;
+		const name = product.stocked ? product.name :
+			<span style={{ color: 'red' }}>
+				{product.name}
+			</span>;
+		
+		return (
+			<tr>
+				<td>{name}</td>
+				<td>{product.price}</td>
+			</tr>
+		);
+	}
+}
 
 
 
